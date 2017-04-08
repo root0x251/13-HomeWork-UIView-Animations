098a05d3ad08259054348d525001ca319dbd5c89
Slava <rtootik@gmail.com>
2017-04-08 21:03:32 +0300

    Initial Commit

---
+++ 13-HomeWork-UIView-Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  13-HomeWork-UIView-Animations
+//
+//  Created by Slava on 08.04.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 13-HomeWork-UIView-Animations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  13-HomeWork-UIView-Animations
+//
+//  Created by Slava on 08.04.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
